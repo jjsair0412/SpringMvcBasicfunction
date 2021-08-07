79675a4d1d69143a7d8fb45adaa80cf9b83f6631
주진성 <jjsair0412@naver.com>
2021-08-07 21:39:06 +0900

    뷰 템플릿을 사용하는방법

---
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,33 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1(){
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data","hello!");
+        return mav;
+    }
+
+    /**
+     * Model에 Attribute를 추가해서 값을 전달하는 코드
+     *
+     */
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data","hello model");
+        return "response/hello";
+    }
+
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model){
+        model.addAttribute("data","hello V3");
+    }
+}
